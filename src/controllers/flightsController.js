e1a481f6501374d9b0f3ae3327167ec30d706f5f
RedoRojo <cristhiabnvictorrm@gmail.com>
2024-10-03 02:11:40 -0400

    Se obtiene un vuelo por el numero del vuelo

--- src/controllers/flightsController.js
+++ src/controllers/flightsController.js
@@ -12,6 +12,20 @@ const getAllVuelos = async (req, res) => {
     }
   };
 
+const getVueloByNumber = async (req, res) => {
+  let number = req.params.vueloNumber
+  console.log(number)
+  try {
+    let querySnapshot = await db.collection(collectionName).where("flightNumber", '==', number).limit(1).get()
+    const vueloDoc = querySnapshot.docs[0]; 
+    const vuelo = vueloDoc.data()
+    res.status(200).json(vuelo)
+  } catch (error) {
+    console.error('Error al obtener el vuelo:', error); 
+    res.status(500).json({error: "Error al obtener el vuelo"});  
+  }
+}
+
   module.exports = {
-    getAllVuelos
+    getAllVuelos, getVueloByNumber
   };
--- src/routes/flights.js
+++ src/routes/flights.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const vuelosController = require('../controllers/flightsController');
 
 router.get('/', vuelosController.getAllVuelos);
+router.get('/:vueloNumber', vuelosController.getVueloByNumber); 
 
 module.exports = router;
