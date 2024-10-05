a145cde7ce613f9d09b28c78bb200d51dcd66625
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-10-04 22:51:40 -0400

    correcciones para el deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,24 @@
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  console.error('FIREBASE_SERVICE_ACCOUNT no está definido en las variables de entorno.');
+  process.exit(1); 
+}
+
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} catch (error) {
+  console.error('Error al parsear FIREBASE_SERVICE_ACCOUNT:', error);
+  process.exit(1); 
+}
+
 initializeApp({
-    credential: applicationDefault(),
+    credential: cert(serviceAccount),
     // databaseURL no es estrictamente necesario para Firestore, pero puedes incluirlo si lo necesitas
     // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
   });
